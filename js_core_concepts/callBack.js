c455227c57350808b746ccac383ad287495d954a
RabaniHosni10 <rabbanihosni10@gmail.com>
2026-02-18 05:14:05 +0600

    Callback function

--- js_core_concepts/callBack.js
+++ js_core_concepts/callBack.js
@@ -1,13 +1,30 @@
-//Callback function
+//callback
+
+
 function pakhiBhai(callMe,patro,patri){
+    console.log('CallMe Perameter',callMe);
+    console.log("Value of patro is:", patro);
     if(patri){
-        pakhiBhai(callMe);
+        // console.log(callMe);
+        callMe(patro);
+
     }
-
+    else{
+        console.log('Or kopal e patri nai!');
+    }
 }
-console.log(pakhiBhai)
 
-function callMe(person){
-    console.log("Calling:", person);
+
+function callSomeOne(person){
+    console.log("Calling",person);
 }
-callMe('Kodu');
+callSomeOne('Modu');
+
+pakhiBhai(callSomeOne,'jodu');
+
+
+// 
+// [1,2,3,4,5].map(x=>x+5)
+
+
+//When a function is called inside another functions argument then its a callback function
